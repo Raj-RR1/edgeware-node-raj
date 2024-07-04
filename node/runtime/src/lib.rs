5dbd1524f2897720ac99dd0dacd2462a593b8bc2
Raj Raorane <rajraorane576@gmail.com>
2024-07-04 16:07:33 +0800

    increment spec versions

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -132,6 +132,7 @@ use pallet_evm::{
 
 /// Runtime version.
 #[cfg(not(feature = "beresheet-runtime"))]
+#[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("edgeware"),
 	impl_name: create_runtime_str!("edgeware-node"),
@@ -140,14 +141,15 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 53,
-	impl_version: 53,
+	spec_version: 54,
+	impl_version: 54,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 2,
-	state_version: 53,
+	transaction_version: 3,
+	state_version: 54,
 };
 
 #[cfg(feature = "beresheet-runtime")]
+#[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("beresheet"),
 	impl_name: create_runtime_str!("beresheet-node"),
